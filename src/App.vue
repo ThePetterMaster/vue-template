bc4a7a5ceb06603a423b173a34fa465ecd3100d1
Pedro Neto <piromaticos@gmail.com>
2023-06-03 21:53:01 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div>
+      <NavBar/>
+      <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+        <div class="px-3 py-2">
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+            ac consectetur ac, vestibulum at eros.
+          </p>
+          <b-img
+            src="https://picsum.photos/500/500/?image=54"
+            fluid
+            thumbnail
+          ></b-img>
+        </div>
+      </b-sidebar>
+    </div>
+    <router-view />
   </div>
 </template>
-
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  components: { NavBar },
+}
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav class="p-2">
+      <b-button v-b-toggle.sidebar-1><i class="bi bi-list"></i></b-button>
+      <b-nav-item href="/">Home</b-nav-item>
+      <b-nav-item href="/about">About</b-nav-item>
+    </b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
+      <b-dropdown
+        id="dropdown-right"
+        right
+        text="Right align"
+        class="m-2"
+        no-caret
+      >
+        <template #button-content>
+          <i class="bi bi-person-fill"></i>
+        </template>
+        <b-dropdown-item href="#">Meu perfil</b-dropdown-item>
+        <b-dropdown-item href="#">Sair</b-dropdown-item>
+      </b-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
