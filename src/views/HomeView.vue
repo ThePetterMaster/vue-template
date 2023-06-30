ac1bb19f2053c4e9e90cb67b8e67ae73a337cfeb
Pedro Neto <piromaticos@gmail.com>
2023-06-30 17:28:59 -0300

    corrigindo carrosel imagem maior

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,15 +10,15 @@
   >
     <b-carousel-slide
       caption="First Slide"
-      img-src="https://picsum.photos/1024/200/?image=10"
+      img-src="https://picsum.photos/1024/300/?image=10"
     ></b-carousel-slide>
     <b-carousel-slide
       caption="Second Slide"
-      img-src="https://picsum.photos/1024/200/?image=12"
+      img-src="https://picsum.photos/1024/300/?image=12"
     ></b-carousel-slide>
     <b-carousel-slide
       caption="Third Slide"
-      img-src="https://picsum.photos/1024/200/?image=22"
+      img-src="https://picsum.photos/1024/300/?image=22"
     ></b-carousel-slide>
   </b-carousel>
 
