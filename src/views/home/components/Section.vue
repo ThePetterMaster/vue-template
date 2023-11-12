8ffdb791b74476e74262f38cc093140cd132a888
ThePetterMaster <piromaticos@gmail.com>
2023-11-11 23:15:36 -0300

    feat: alterando padding da section da home

--- src/views/home/components/Section.vue
+++ src/views/home/components/Section.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="titulo">
-    <div class="py-5 px-2">
+    <div class="py-4 px-2">
       <h1 class="text-center texto-cor-especial fonte-titulo">Blog pessoal</h1>
       <p class="text-center text-secondary mb-7">
         Blog pessoal que trata de assuntos de interesse do desenvolvedor
